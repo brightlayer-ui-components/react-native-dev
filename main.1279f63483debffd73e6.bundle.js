61bd5e5a790242cdce44c8900c2316d74463b8b4
Brightlayer UI Agent <brightlayer-ui@eaton.com>
2024-06-06 12:32:36 +0000

    Automated deployment to GitHub Pages 5907287be1d3488f0bb7aca2e4f38f690104dfe4

--- main.1279f63483debffd73e6.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{417:function(n,o,p){p(418),p(570),n.exports=p(1256)},482:function(n,o){},526:function(n,o){}},[[417,1,2]]]);
---
+++ vite.config.ts
@@ -0,0 +1,60 @@
+/// <reference types='vitest' />
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
+import {nxViteTsPaths} from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import mdx from '@mdx-js/rollup';
+import remarkGfm from 'remark-gfm';
+
+export default defineConfig({
+  root: __dirname,
+  cacheDir: '../node_modules/.vite/docs',
+
+  server: {
+    port: 4200,
+    host: 'localhost',
+    open: true,
+  },
+
+  preview: {
+    port: 4300,
+    host: 'localhost',
+  },
+
+  plugins: [
+    react(),
+    nxViteTsPaths(),
+    mdx({
+      /* jsxImportSource: …, otherOptions… */
+      remarkPlugins: [remarkGfm],
+      providerImportSource: '@mdx-js/react',
+    }),
+  ],
+
+  // Uncomment this if you are using workers.
+  // worker: {
+  //  plugins: [ nxViteTsPaths() ],
+  // },
+
+  build: {
+    outDir: '../dist/docs',
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+
+  test: {
+    globals: true,
+    cache: {
+      dir: '../node_modules/.vitest',
+    },
+    environment: 'jsdom',
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+
+    reporters: ['default'],
+    coverage: {
+      reportsDirectory: '../coverage/docs',
+      provider: 'v8',
+    },
+  },
+});
